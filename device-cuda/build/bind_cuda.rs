de230481b67a52e14e5b15475585363b5230204b
Andy Yang <71013200+Y-jiji@users.noreply.github.com>
2022-11-16 11:18:22 +0800

    Add: build script

---
+++ device-cuda/build/bind_cuda.rs
@@ -0,0 +1,36 @@
+extern crate bindgen;
+use std::env::current_dir;
+use std::error::Error; 
+use std::path::*;
+use std::fs::*;
+
+const CUDA_INCLUDE_PATH: &str = env!(
+    "CUDA_INCLUDE_PATH", 
+    "In build script: Environment variable CUDA_INCLUDE_PATH is missing. It should be where \"cuda.h\" file lives"
+);
+
+const CUDA_LIBRARY_PATH: &str = env!(
+    "CUDA_LIBRARY_PATH",
+    "In build script: Environment variable CUDA_LIBRARY_PATH is missing. It should be where \"cuda.lib\" or \"libcuda.a\" file lives"
+);
+
+const CUDA_BINDING_PATH: &str = "cu-bind";
+
+pub fn bind_cuda(
+    include: &str, 
+    writeto: &str,
+) -> Result<(), Box<dyn Error>> {
+    let cd = current_dir()?;
+    let include_path = PathBuf::new().join(CUDA_INCLUDE_PATH).join(include);
+    let writeto_path = PathBuf::new().join(cd).join(CUDA_BINDING_PATH).join(writeto);
+    let mut builder = bindgen::Builder::default();
+    builder = builder
+        .clang_args(["-I", CUDA_INCLUDE_PATH])
+        .clang_args(["-L", CUDA_LIBRARY_PATH]);
+    builder = builder.header(include_path.to_str().unwrap());
+    let binding = builder.generate()?;
+    assert!(writeto_path.exists() && writeto_path.is_file(), "There is no file {writeto_path:?}");
+    let writeto_file = Box::new(File::create(&writeto_path).expect(&format!("Cannot open file {writeto_path:?}")));
+    binding.write(writeto_file).expect(&format!("Cannot write to {writeto_path:?}"));
+    Ok(())
+}
--- device-cuda/build/main.rs
+++ device-cuda/build/main.rs
@@ -1,5 +1,14 @@
 use std::error::Error;
 
+mod bind_cuda;
+mod bind_nvptx;
+
+use bind_cuda::*;
+// use bind_nvptx::*;
+
 fn main() -> Result<(), Box<dyn Error>> {
+    bind_cuda("cuda_runtime.h", "cuda-rt.rs")?;
+    bind_cuda("cuda.h", "cuda-driver.rs")?;
+    bind_cuda("cublas.h", "cublas.rs")?;
     Ok(())
 }
